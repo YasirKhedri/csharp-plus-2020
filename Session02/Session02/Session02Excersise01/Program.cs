2a0b455c5a793078b652668c853360d2b35d6c9d
yasir_khedri@hotmail.com <yasir_khedri@hotmail.com>
2020-08-26 10:32:52 +0200

    back to original

--- Session02/Session02/Session02Excersise01/Program.cs
+++ Session02/Session02/Session02Excersise01/Program.cs
@@ -7,7 +7,7 @@ namespace Session02Excersise01
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("test");
+            
             
             
         }
