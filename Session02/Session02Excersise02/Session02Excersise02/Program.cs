ab23d9c439bc58cf6314d3ef00b432c7fb272c8e
yasir_khedri@hotmail.com <yasir_khedri@hotmail.com>
2020-08-26 11:46:36 +0200

    If-sats

---
+++ Session02/Session02Excersise02/Session02Excersise02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Session02Excersise02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello dot you want to enter yoyr name? (y/n) ");
+            var key = Console.ReadKey();
+            if (key.KeyChar == 'n')
+                return;
+            Console.WriteLine("Enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("hello, " + name );
+            Console.ReadKey();
+
+
+        }
+    }
+}
